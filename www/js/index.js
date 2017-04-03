65fc2e56fe0893cb6f59c204c50cea0256c28126
Ing. Andres Segreda <Ing. Andres Segreda>
2017-04-02 22:16:26 -0600

    test pouch

--- www/js/index.js
+++ www/js/index.js
@@ -122,7 +122,24 @@ var app = {
     },
 	
 	pouchTest: function(){
-		db = new PouchDB('http://192.168.1.3:5984/condominus');
+		db = new PouchDB('condominus');
+		db.sync('mydb', 'http://localhost:5984/mydb', {
+			  live: true,
+			  retry: true
+			}).on('change', function (info) {
+			  alert("cambio")
+			}).on('paused', function (err) {
+			  // replication paused (e.g. replication up to date, user went offline)
+			}).on('active', function () {
+			  // replicate resumed (e.g. new changes replicating, user went back online)
+			}).on('denied', function (err) {
+			  // a document failed to replicate (e.g. due to permissions)
+			}).on('complete', function (info) {
+			  // handle complete
+			}).on('error', function (err) {
+			  // handle error
+			});
+
 		db.put({
 		  _id: 'mydoc',
 		  title: 'Heroes'
