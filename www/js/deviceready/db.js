a96b28787eade7acb1a288c1886c8562783c4fec
Ing. Andres Segreda <Ing. Andres Segreda>
2017-06-05 11:00:44 -0600

    Theme.Holo.Light

--- www/js/deviceready/db.js
+++ www/js/deviceready/db.js
@@ -1,3 +1,21 @@
 function onDeviceReady_db(){
 	db = new PouchDB('condominus', {adapter : 'websql'});
+	
+	db.upsert = function(id,data,callback,err){
+		db.get(id).then(function(doc) {
+			  return db.put(Object.assign({
+				_id: id,
+				_rev: doc._rev
+			  },data));
+			}).then(function(response) {
+			  console.log(response)
+			}).catch(function (err) {
+			  if(err.status = 404){
+				 return  db.put(Object.assign({
+				_id: id,
+			  },data));
+			  }
+			});
+		
+	}
 }
