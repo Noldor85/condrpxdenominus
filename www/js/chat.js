cc35a922fe73f9a89b846e36d40528bcd36a92d9
Macbook Pro <an@example.com>
2017-06-07 12:41:40 -0600

    chats working

--- www/js/chat.js
+++ www/js/chat.js
@@ -13,3 +13,33 @@ $("#chat_sender_btn").tapend(function(){
 		$("#chat_lst_box").append('<div class="chat_message"><div class="i_said">'+$("#chat_sender_txt").text()+'<div class="said_date">20/01/2010 15:20</div></div></div>');
 	}
 });
+
+
+function makeChatSwipe (selector){
+	$(selector).swipe({
+		swipeLeft:function(event, direction, distance, duration, fingerCount) {
+			$(".chat_lst_element").not($(this)).animate({"margin-left" : 0+"px"}); 
+			var thisMarginLeft = 0-parseInt($(this).css("margin-left").replace("px",""));
+			if(thisMarginLeft > 30){
+				$(this).animate({"margin-left" :-70+"px"});
+			}else{
+				$(this).animate({"margin-left" : 0+"px"});
+			}
+		},
+		swipeRight:function(event, direction, distance, duration, fingerCount){
+			$(this).animate({"margin-left" :0+"px"});
+		},
+		
+		swipeStatus:function(event, phase, direction, distance, duration, fingers, fingerData, currentDirection){
+			if(direction == "left"){
+				if(distance > 0 & distance < 70){
+					$(this).css({"margin-left" : "-" + distance + "px"})
+				}
+			}
+		 },
+		  allowPageScroll:"vertical",
+			threshold:5
+	})
+}
+
+makeChatSwipe(".chat_lst_element")
